d2641ef65071af5aa0580bf49d08aa45121d8b33
Igor <98655982+IgorBroch@users.noreply.github.com>
2024-02-11 17:13:10 -0300

    __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -2,13 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 
 app = Flask(__name__)
 
 
 app.config['SECRET_KEY'] = '8dc7e344a750a3ca9f038a9d2946d311'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///IB.db'
+if os.getenv("DATABASE_URL"):
+  app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else: 
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///IB.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 database = SQLAlchemy(app)
